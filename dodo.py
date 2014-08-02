26a1c88a3b2378d36abc523cd4d3da7a6701494e
schettino72 <schettino72@gmail.com>
2014-08-03 00:27:01 +0800

    added karma, package.json and moved things around

--- dodo.py
+++ dodo.py
@@ -1,5 +1,20 @@
+from glob import glob
+
 from doitweb import JsHint
+
+
+KARMA_CMD = 'karma start karma.conf.js --single-run'
+SRC_FILES = glob('src/*.js')
+TEST_FILES = glob('test/*.js')
+
 
 def task_jshint():
     hint = JsHint(config='hint.json')
-    yield hint.tasks(['*.js'])
+    yield hint.tasks(SRC_FILES)
+
+
+def task_test():
+    return {
+        'actions': [KARMA_CMD],
+        'file_dep': SRC_FILES + TEST_FILES,
+        }
